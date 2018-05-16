1021702953ddbe90ddca92f4e1b5cd94527ef421
carlashub <carlawlondon@gmail.com>
2018-05-16 13:28:12 +0100

    first commit-under construction

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,4 +6,10 @@ e.stopPropagation();
 $('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
   $('html').toggleClass('nav-open');
 });
-$
+
+$(function(){
+  setTimeout(function(){
+    $('.text-animation').removeClass('hidden').delay(4000).fadeOut('slow');
+  }, 500);
+  
+});

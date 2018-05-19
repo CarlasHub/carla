9176a94461856a29fb1a4fb086e1a4bcf2b7f6ec
carlashub <carlawlondon@gmail.com>
2018-05-19 14:29:51 +0100

    clean up the extra syntax after failed merge

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 $('.toggled-nav-wrapper').on('click', function(e){
 e.stopPropagation();
@@ -12,21 +12,5 @@ $(function(){
   setTimeout(function(){
     $('.text-animation').removeClass('hidden').delay(4000).fadeOut('slow');
   }, 500);
+});
   
-=======
-
-$('.toggled-nav-wrapper').on('click', function(e){
-e.stopPropagation();
-});
-
-$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
-  $('html').toggleClass('nav-open');
-});
-
-$(function(){
-  setTimeout(function(){
-    $('.text-animation').removeClass('hidden').delay(4000).fadeOut('slow');
-  }, 500);
-  
->>>>>>> 645c546d4ace8b350d6155c6257bec7bc2e1b075
-});
